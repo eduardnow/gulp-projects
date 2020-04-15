d00e6cd9d78729fbbe1c5c68e2253f21d5fe96e6
Eduard Castro <eduard.cas07@gmail.com>
2020-04-15 14:45:37 -0500

    Add gulp sass example

--- gulp-intro/gulpfile.js
+++ gulp-intro/gulpfile.js
@@ -1,1 +1,22 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('hello', function () {
+    console.log('Hello');
+});
+
+gulp.task('world', ['hello'], function () {
+    console.log('World');
+});
+
+gulp.task('sass', function () {
+    return gulp.src('style.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('tmp'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('style.sass', ['sass']);
+});
+
+gulp.task('default', ['hello', 'world', 'sass']);

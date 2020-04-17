606b92923fde09ca91f43ff82206a01e34d37ced
Eduard Castro <eduard.cas07@gmail.com>
2020-04-17 18:15:33 -0500

    Add gulp autoprefixer and exception handling gulp sass

--- gulp-intro/gulpfile.js
+++ gulp-intro/gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('hello', function () {
     console.log('Hello');
@@ -15,17 +16,21 @@ gulp.task('world', ['hello'], function () {
 gulp.task('sass', function () {
     return gulp.src('src/style.sass')
         .pipe(sourcemaps.init())
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('.tmp'))
         .pipe(reload({ stream: true }));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch('src/style.sass', ['sass']);
 });
 
-gulp.task('serve', ['sass'],function() {
+gulp.task('serve', ['sass'], function () {
     browserSync({
         server: {
             baseDir: ['.tmp', 'src'],

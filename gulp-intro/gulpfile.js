b7bcba5b425f2b756393050ba906756c8bfe304e
Eduard Castro <eduard.cas07@gmail.com>
2020-04-18 19:51:31 -0500

    Add build process tasks for production

--- gulp-intro/gulpfile.js
+++ gulp-intro/gulpfile.js
@@ -5,6 +5,7 @@ var reload = browserSync.reload;
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var pug = require('gulp-pug');
+var uglify = require('gulp-uglify');
 
 gulp.task('hello', function () {
     console.log('Hello');
@@ -12,19 +13,6 @@ gulp.task('hello', function () {
 
 gulp.task('world', ['hello'], function () {
     console.log('World');
-});
-
-gulp.task('sass', function () {
-    return gulp.src('src/style.sass')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('.tmp'))
-        .pipe(reload({ stream: true }));
 });
 
 gulp.task('watch', function () {
@@ -43,10 +31,47 @@ gulp.task('serve', ['sass', 'pug'], function () {
     gulp.start('watch');
 });
 
+gulp.task('sass', function () {
+    return gulp.src('src/style.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('.tmp'))
+        .pipe(reload({ stream: true }));
+});
+
 gulp.task('pug', function () {
     return gulp.src('src/*.pug')
         .pipe(pug())
         .pipe(gulp.dest('.tmp'))
 })
 
+gulp.task('sass:prod', function () {
+    return gulp.src('src/style.sass')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('pug:prod', function () {
+    return gulp.src('src/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('js', function () {
+    gulp.src('./src/app.js')
+        .pipe(uglify({ compress: true }))
+        .pipe(gulp.dest('dist'));
+});
+
 gulp.task('default', ['hello', 'world', 'sass']);
+
+gulp.task('build', ['sass:prod', 'pug:prod', 'js']);
